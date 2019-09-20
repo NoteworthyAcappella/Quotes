1a9a76e661b5f6e191ae0410217f520e6c6a1812
Noteworthy A Cappella LHS <38771589+NoteworthyAcappella@users.noreply.github.com>
2019-09-20 15:49:55 -0400

    Update quoteLoader.js

--- quoteLoader.js
+++ quoteLoader.js
@@ -3,7 +3,7 @@ function loadQuotes(){
   quotes.onreadystatechange = function(){
     if (this.readyState == 3 && this.status == 200) {
       var myObj = JSON.parse(this.responseText);
-      document.getElementById("bod").innerHTML = document.getElementById("bod").innerHTML + "</p><p>" + quotes.who[0];
+      document.getElementById("bod").innerHTML = document.getElementById("bod").innerHTML + "</p><p>" + quotes.who;
     }
   };
   quotes.open("GET", "Data.json", true);
